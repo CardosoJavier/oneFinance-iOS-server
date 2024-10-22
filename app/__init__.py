21b7e4e7869d1d1bfa6fbcec5e8d0b2ef9bdfaa4
Javier Cardoso <cardoso.javier@outlook.com>
2024-10-21 19:27:12 -0500

    change server to its own repo

---
+++ app/__init__.py
@@ -0,0 +1,51 @@
+import os
+import logging
+import argparse
+from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+from app.controller.clientRoutes import tellerRoutes
+
+load_dotenv("../.env")
+
+def _parse_args():
+    parse = argparse.ArgumentParser(description="Teller environment config")
+
+    # add arguments
+    parse.add_argument('--environment', default='sandbox', choices=['sandbox', 'dev', 'prod'], help="Entern environment")
+    parse.add_argument('--cert', type=str, help="mTLS Certificate path")
+    parse.add_argument('--key', type=str, help="mTLS private key")
+
+    args = parse.parse_args()
+
+    # check if cert and key are needed
+    need_cert = args.environment in ['dev', 'prod']
+    has_cer = args.cert and args.key
+
+    if need_cert and not has_cer:
+        parse.error("--cert and --key are requiered when --environment is not sandbox")
+
+    return args
+
+def create_app():
+
+    # get args from command line
+    #args = _parse_args()
+    #cert = (args.cert, args.key)
+
+    # app instance
+    app = Flask(__name__)
+
+    # CORS configuration. Origins will be modified later on
+    CORS(app, origins='*')
+
+    # loggin settings
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    # routes
+    app.register_blueprint(tellerRoutes)
+
+    # Set up app environment
+    logging.info("starting oneFinance server...")
+    
+    return app
---
+++ app/controller/clientRoutes.py
@@ -0,0 +1,38 @@
+import logging
+from flask import Blueprint, request
+from app.service.tellerService import TellerService
+
+tellerRoutes = Blueprint('tellerRoutes', __name__)
+
+tellerService: TellerService = TellerService()
+
+# Fetch accounts
+@tellerRoutes.route('/api/accounts', methods=['GET'])
+def fetchAccounts():
+    return tellerService.fetchAccounts()
+
+# Fetch balances
+@tellerRoutes.route('/api/balances', methods=['GET'])
+def fetchAccountBalances():
+    account_id = request.args.get("account_id")
+
+    if account_id:
+        return tellerService.fetchAccountBalance(account_id)
+    else:
+        logging.error("user id missing in request")
+        return {"error": "user id missing in request"}
+        
+
+# Fetch access token
+@tellerRoutes.route('/api/storeAccessToken', methods=['POST'])
+def storeAccessToken():
+    requestData = request.json
+    
+    if requestData.get("access_token"):
+        tellerService.storeAccessToken(requestData.get("access_token"))
+        logging.info("Token saved succesfully")
+        return {"status": "success getting token"}
+    
+    else:
+        logging.error("Token was not in request")
+        return {"status": "failed getting token"}
